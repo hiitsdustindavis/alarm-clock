0615d617da3a492d8344bf7b0e11168d3168b6e0
Dustin Davis and Trevor Elvey <hiitsdustindavis@gmail.com>
2016-03-15 16:44:15 -0700

    try to fix time compare

--- build/js/app.js
+++ build/js/app.js
@@ -3698,14 +3698,37 @@ exports.update = function () {
 
 }));
 },{}],3:[function(require,module,exports){
-
 var update = require('./../js/time-interface.js').update;
-// UPDATE THE PINGPONG REQUIRE ABOVE
+
+setAlarm = function(input) {
+  var save = input;
+  var currentTime = moment().format('h:mm:ss a');
+  if(save.toString() === currentTime.toString())
+  {
+    return $("body").css("background-color", "red");
+  }
+}
+
 $(document).ready(function(){
   var input = null;
 
     setInterval(update, 1000);
-});
+    $('#submit-alarm').submit(function(event){
+      event.preventDefault();
+      input = $('#input-alarm').val();
+      alarm = setAlarm(input);
+
+      $('#submit-alarm').append("<p>"+input+"</p>");
+      setInterval(function(){
+        var currentTime = moment().format('h:mm:ss a');
+        if(input === currentTime)
+        {
+          return $(".body").css("background-color", "red");
+        }
+      }, 1000);
+    });
+
+  });
 
 var moment = require('moment');
 
